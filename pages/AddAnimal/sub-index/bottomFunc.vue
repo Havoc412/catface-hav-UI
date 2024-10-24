03aa1e54aa241bb9ca90ce26dbd828dc63eafb81
Havoc412 <2993167370@qq.com>
2024-10-24 19:40:48 +0800

    special func

--- pages/AddAnimal/sub-index/bottomFunc.vue
+++ pages/AddAnimal/sub-index/bottomFunc.vue
@@ -5,7 +5,11 @@
             <view>标记地点</view>
             <view class="shrink"></view>
             <h-btn variant="tonal">
-                经度：xxx；纬度：xxx
+                <view v-if="!flag.poi" class="why flex-center-horizontal gap-5">
+                    <h-icon name="com-light" size="16"/>
+                    标记位置让信息更准确！
+                </view>
+                <view v-else>经度：xxx；纬度：xxx</view>
             </h-btn>
             <h-icon name="arrow-right"/>
         </view>
@@ -13,13 +17,20 @@
             <h-icon name="catface-paw" size="20"/>
             <view>录入猫脸</view>
             <view class="shrink"></view>
+            <h-btn variant="tonal">
+                <view v-if="!flag.catface" class="why flex-center-horizontal gap-5">
+                    <h-icon name="com-light" size="16"/>
+                    录入面部模板启用。
+                </view>
+                <view v-else>模板评估：xx.xx</view>
+            </h-btn>
             <h-icon name="arrow-right"/>
         </view>
     </view>
 </template>
 
 <script setup>
-    import { ref } from "vue";
+    import { reactive } from "vue";
 
     import { GetPoi } from "../../../utils/poi";
     // store
@@ -29,14 +40,26 @@
     });
     const emits = defineEmits(['getPoi']);
 
+    const flag = reactive({
+        poi: false,
+        catface: false,
+    })
+    
+    const date = reactive({
+        // TODO 存储得到的数据，解析后展示在前端。
+    })
 // FUNC
     function getPoi() {
         // TODO 鉴定权限
         GetPoi();
-    }
+    };
 
 </script>
 
 <style scoped>
+
+.why {
+    font-size: 12px;
+}
 
 </style>        
