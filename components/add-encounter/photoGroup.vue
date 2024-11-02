811ce5c1661caa34fe33589fc6541a129808c49f
Havoc412 <2993167370@qq.com>
2024-11-02 23:41:47 +0800

    🆕start add encounter

---
+++ components/add-encounter/photoGroup.vue
@@ -0,0 +1,122 @@
+<template>
+    <scroll-view scroll-x="true">
+        <view class="flex-horizontal gap-5 pd-15">
+            <view v-if="props.mode == 'add'" class="add-container flex-center-both" :style="{
+                '--size': props.size + 'px',
+                '--radius': props.radius + 'px',
+            }" @click="addImage">
+                <up-icon name="plus" :color="color['main-deep']" size="30"></up-icon>
+            </view>
+            <template v-for="(item, index) in props.imgList" :key="index">
+                <up-image lazy-load :src="item" :radius="props.radius" :height="props.size + 'px'" :width="props.size + 'px'"/>
+            </template>
+        </view>
+    </scroll-view>
+</template>
+
+<script setup>
+    import { ref, reactive } from "vue";
+
+    import color from "@/css/theme/index.module.scss";
+    import api from "../../request/photo";
+    // store
+// DATA
+    const props = defineProps({
+        mode: {
+            type: String,
+            default: "default" // add
+        },
+        imgList: {
+            type: Array,
+            default: () => [
+                '/static/Qcat.png',
+                '/static/user.jpg',
+                '/static/Qcat.png',
+            ]
+        },
+        size: {
+            type: [Number, String],
+            default: 160
+        },
+        radius: {
+            type: [Number, String],
+            default: 15
+        },
+		//  When type == 'add' 时有效；
+		imageMaxNum: { // 上传数目上限
+			type: Number,
+			default: 5
+		}
+    });
+    const emits = defineEmits(['addImage']);
+
+    const status = reactive({
+		upload: 'nothing',
+	})
+
+    const flag = reactive({
+		preview: false,
+	})
+
+// FUNC
+
+   	// 添加图片
+	async function addImage() {
+		try {
+			status.upload = 'loadding';
+			const res = await chooseImageWrapper({
+				count: props.imageMaxNum - props.imgList.length,
+				sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+				sourceType: ['album'] // 从相册选择
+			});
+			status.upload = res;
+		} catch (err) {
+			console.error(err);
+		}
+	}
+
+	// 封装 chooseImage 为返回 Promise 的函数
+	function chooseImageWrapper(options) {  // UPDATE 或许这个函数应该交给外面来做。
+		return new Promise((resolve, reject) => {
+			uni.chooseImage({
+				...options,
+				success: async function (res) {
+					const files = res.tempFilePaths;
+					const paths = await api.UploadEncounterPhotos(files); // INFO 重点业务接口
+					emits('addImage', paths);
+					resolve('nothing'); // resolve
+				},
+				fail: function (err) {
+					console.error(err);
+					uni.showToast({
+                        title: '图片选择失败',
+                        icon: 'none'
+					});
+					status.value.upload = 'err';
+					reject('err'); // 失败时 reject
+				},
+				complete: function (res) {
+					// BUG 也无法收到 PC 端的 ‘取消’
+					console.debug(res);
+				}
+			});
+		});
+	}
+
+	function showImage() {
+		flag.preview = true;
+	}
+
+</script>
+
+<style scoped>
+
+.add-container {
+    width: var(--size);
+    height: var(--size);
+    border-radius: var(--radius);
+
+    background-color: #ffffffd0;
+}
+
+</style>        
--- pages/AddEncounter/index.vue
+++ pages/AddEncounter/index.vue
@@ -1,19 +1,48 @@
 <template>
-    <view>
-        
+    <view class="flex-center-vertical container-top">
+        <photoGroup mode="add" :imgList="dataShow.photos" @addImage="addImage"/>
+        <up-divider :hairline="true" lineColor="#000"/>
+        123
     </view>
 </template>
 
 <script setup>
-    import { ref } from "vue";
+    import { reactive } from "vue";
+
+    import nginx from "../../request/nginx";
     // com
+    import photoGroup from "../../components/add-encounter/photoGroup.vue";
     // store
 // DATA
 
+    const data = reactive({
+        photos: [],
+    })
+
+    const dataShow = reactive({
+        photos: [],
+    })
+
 // FUNC
+    function addImage(paths) {
+        if(!paths)  return;
+        data.photos = data.photos.concat(paths);
+        
+        // 展示用临时路径。
+        const newFullPath = paths.map(path => {
+            let fullpath = nginx.encounterPhotos(path)
+            console.debug(fullpath);
+            return fullpath;
+        })
+        
+        dataShow.photos = dataShow.photos.concat(newFullPath);
+        console.debug(dataShow.photos, data.photos);
+    }
+
 
 </script>
 
 <style scoped>
+
 
 </style>        
