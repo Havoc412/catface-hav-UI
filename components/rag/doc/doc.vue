61e64cae38505c7e25556f66fa2d9b0a7b41c204
Havoc412 <2993167370@qq.com>
2024-11-20 21:55:47 +0800

    refactor(components): 优化参考资料标题显示逻辑
    - 使用 ref 替代常量字符串，实现动态标题
    - 添加 onMounted 钩子，根据参考资料类型动态设置标题
    - 优化模板结构，调整元素顺序和样式

--- components/rag/doc/doc.vue
+++ components/rag/doc/doc.vue
@@ -1,7 +1,7 @@
 <!--这里集中管理不同种类的 doc，直接被 ai.vue 引入，并调用其方法-->
 <template>
-    <view class="mt-10">
-        <up-divider :hairline="false" :text="TITLE" lineColor="#888888"/>
+    <up-divider :hairline="false" :text="TITLE" lineColor="#888888"/>
+    <view class="mt-10 flex-vertical gap-10">
         <template v-for="(item, index) in nodes" :key="index">
             <knowledge v-if="item.type == 'doc'"
                 :id="item.id"
@@ -20,12 +20,17 @@
 </template>
 
 <script setup>
-    import { ref } from "vue";
+    import { ref, onMounted } from "vue";
     // com
     import knowledge from "./knowledge.vue";
     import encounter from "./encounter.vue";
 
-    const TITLE = "参考资料"
+    const TITLE = ref("参考资料")
+
+    let flag = {
+        knowledge: false,
+        encounter: false
+    }
     
     // store
 // DATA
@@ -38,6 +43,22 @@
     const emits = defineEmits([]);
 
 // FUNC
+    onMounted(() => {  // 很粗糙的 if-else
+        props.nodes.forEach(item => {
+            if(item.type == "doc")
+                flag.knowledge = true;
+            else if(item.type == "encounter")
+                flag.encounter = true;
+        })
+        if(!flag.knowledge && !flag.encounter)
+            TITLE.value = "暂无参考资料"
+        else if(flag.knowledge && !flag.encounter)
+            TITLE.value = "参考资料"
+        else if(!flag.knowledge && flag.encounter)
+            TITLE.value = "参考路遇"
+        else
+            TITLE.value = "参考信息"
+    })
     
 </script>
 
